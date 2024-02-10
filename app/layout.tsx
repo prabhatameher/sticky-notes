78ce3294d3948bc8cc9a14a70fbde85ceedb0cc2
prabhatameher <meher.prabhat1996@gmail.com>
2024-02-10 22:37:30 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Kalam:wght@300;400;700&display=swap" rel="stylesheet" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
---
+++ app/sticky-notes/page.tsx
@@ -0,0 +1,229 @@
+"use client"
+import { format } from 'date-fns';
+import { Check, X } from 'lucide-react';
+import React, { useEffect, useState } from 'react'
+
+interface Note {
+    id: number;
+    title: string;
+    description: string;
+    color?: string;
+    dateTime: Date
+}
+
+
+const NoteBackgroundColorVariants = {
+    sky: 'bg-sky-100',
+    pink: 'bg-pink-100',
+    yellow: 'bg-yellow-100',
+    green: 'bg-green-100',
+    lime: 'bg-lime-100',
+    cyan: 'bg-cyan-100',
+    indigo: 'bg-indigo-100',
+    purple: 'bg-purple-100',
+    teal: 'bg-teal-100',
+    rose: 'bg-rose-100',
+    fuchsia: 'bg-fuchsia-100',
+}
+
+const backgroundColorVariants = {
+    sky: 'bg-sky-400',
+    pink: 'bg-pink-400',
+    yellow: 'bg-yellow-400',
+    green: 'bg-green-400',
+    lime: 'bg-lime-400',
+    cyan: 'bg-cyan-400',
+    indigo: 'bg-indigo-400',
+    purple: 'bg-purple-400',
+    teal: 'bg-teal-400',
+    rose: 'bg-rose-400',
+    fuchsia: 'bg-fuchsia-400',
+}
+
+const type = {
+    bg: 'bg-',
+    text: 'text-',
+    border: 'border-'
+}
+
+const colors = {
+    yellow: 'yellow-',
+    sky: 'sky-',
+    pink: 'pink-',
+    green: 'green-',
+    lime: 'lime-',
+    cyan: 'cyan-',
+    indigo: 'indigo-',
+    purple: 'purple-',
+    teal: 'teal-',
+    rose: 'rose-',
+    fuchsia: 'fuchsia-'
+}
+
+const range = {
+    50: 50,
+    100: 100,
+    200: 200,
+    300: 300,
+    400: 400,
+    500: 500,
+    600: 600,
+    700: 700,
+    800: 800,
+    900: 900,
+}
+
+const buttonBackgroundColorVariants = {
+    sky: 'bg-sky-600',
+    pink: 'bg-pink-600',
+    yellow: 'bg-yellow-600',
+    green: 'bg-green-600',
+    lime: 'bg-lime-600',
+    cyan: 'bg-cyan-600',
+    indigo: 'bg-indigo-600',
+    purple: 'bg-purple-600',
+    teal: 'bg-teal-600',
+    rose: 'bg-rose-600',
+    fuchsia: 'bg-fuchsia-600',
+}
+
+const colorsList = ["yellow", 'sky', 'pink', 'green', 'lime', 'cyan', 'indigo', 'purple', 'teal', 'rose', 'fuchsia']
+
+const StickyNotes = () => {
+
+    const [notesList, setNotesList] = useState<Note[]>([])
+
+    const [newNoteTitle, setNewNoteTitle] = useState<string>('')
+    const [newNoteDescription, setNewNoteDescription] = useState<string>('')
+    const [selectedColor, setSelectedColor] = useState<string>('yellow')
+
+    const getColor = (classname: string): string => {
+        return classname.toString();
+    }
+
+    useEffect(() => {
+        const storedNotes = localStorage.getItem('notes');
+
+        if (storedNotes) {
+            try {
+                const parsedNotes = JSON.parse(storedNotes) as Note[];
+                setNotesList(parsedNotes);
+            } catch (error) {
+                console.error('Error parsing notes from localStorage:', error);
+                localStorage.removeItem('notes');
+                setNotesList([]);
+            }
+        } else {
+            setNotesList([]);
+        }
+    }, []);
+
+    const addNewNote = () => {
+        if (!!newNoteDescription.trim().length) {
+            const storedNotes = localStorage.getItem('notes');
+            try {
+                let updateNoteList: Note[];
+
+                if (storedNotes) {
+                    const parsedNotes = JSON.parse(storedNotes) as Note[];
+                    updateNoteList = [
+                        { id: parsedNotes[0]?.id + 1, title: newNoteTitle, description: newNoteDescription, color: selectedColor, dateTime: new Date() },
+                        ...parsedNotes,
+                    ];
+                } else {
+                    updateNoteList = [{ id: 0, title: newNoteTitle, description: newNoteDescription, color: selectedColor, dateTime: new Date() }];
+                }
+
+                setNotesList(updateNoteList);
+                localStorage.setItem('notes', JSON.stringify(updateNoteList));
+
+                setNewNoteDescription('');
+                setNewNoteTitle('');
+            } catch (error) {
+                console.error('Error adding new note:', error);
+            }
+        }
+    };
+
+    const removeNote = (noteId: number) => {
+        const storedNotes = localStorage.getItem('notes');
+        try {
+            let updateNoteList: Note[];
+
+            if (storedNotes) {
+                const parsedNotes = JSON.parse(storedNotes) as Note[];
+                updateNoteList = parsedNotes.filter((note) => note.id !== noteId)
+            } else {
+                updateNoteList = [];
+            }
+
+            setNotesList(updateNoteList);
+            localStorage.setItem('notes', JSON.stringify(updateNoteList));
+        } catch (error) {
+            console.error('Error remove note:', error);
+        }
+    }
+
+
+    return (
+        <div className='w-100 min-h-screen' style={{ backgroundImage: "url('brick-wall-painted-white.jpg')" }}>
+            <h1 className='text-center font-bold text-lg pt-2'></h1>
+            <div className='mt-5 flex flex-row flex-wrap justify-center lg:justify-start lg:mx-5'>
+                <div className='h-80 w-[350px] m-2 border relative shadow-md'>
+                    <div className='w-100 h-8 flex justify-evenly items-center bg-white'>
+                        {colorsList.map((color) =>
+                            <button key={color}
+                                className={`${backgroundColorVariants[color]} h-6 w-6 transition-transform hover:scale-150 hover:z-10`}
+                                onClick={() => setSelectedColor(color)}>&nbsp;</button>)}
+                    </div>
+                    <div className='h-8'>
+                        <input
+                            autoFocus
+                            placeholder='Note title'
+                            maxLength={50}
+                            value={newNoteTitle}
+                            onChange={(e) => setNewNoteTitle(e.target.value)}
+                            className={`w-full h-full p-2 outline-none text-center focus:outline-none placeholder:text-sm border-b-2 border-b-white ${NoteBackgroundColorVariants[selectedColor]} kalam-regular`}
+                        // style={{ borderBottom: "2px solid gray", }}
+                        />
+                    </div>
+                    <div className='h-64'>
+                        <textarea
+                            autoFocus
+                            placeholder='write your note here'
+                            maxLength={500}
+                            value={newNoteDescription}
+                            onChange={(e) => setNewNoteDescription(e.target.value)}
+                            className={`w-full h-full p-2 outline-none resize-none focus:outline-none placeholder:text-sm ${NoteBackgroundColorVariants[selectedColor]} kalam-regular`}
+                        />
+                    </div>
+                    {!!newNoteDescription.trim().length &&
+                        <button
+                            className={`absolute right-2 bottom-2 p-4 md:p-2 focus:outline-none  text-white border-white rounded-full cursor-pointer ${buttonBackgroundColorVariants[selectedColor]}`}
+                            onClick={addNewNote}><Check /></button>
+                    }
+                </div>
+                {notesList.map((note) =>
+                    <div key={note.id} className='h-80 w-[350px] m-2 relative shadow-md overflow-hidden transition-transform'>
+                        <div className={`w-100 h-8 flex justify-between items-center px-2 ${backgroundColorVariants[note.color]} text-white `}>
+                            {/* <h6 className='text-sm font-bold font-mono'>{format(new Date(note.dateTime), 'EEE MMM dd hh:mm a')}</h6> */}
+                            <h6 className='font-mono'>{format(new Date(note.dateTime), 'EEE MMM dd hh:mm a')}</h6>
+                            <button
+                                className='absolute top-1 right-2'
+                                onClick={() => removeNote(note.id)}><X /></button>
+                        </div>
+                        <div className={`${NoteBackgroundColorVariants[note.color]} h-72 overflow-y-scroll relative`}>
+                            <h2 className='text-lg text-center mt-1 underline underline-offset-2 capitalize font-semibold kalam-regular'>{note.title ? note.title : null}</h2>
+                            <h6 className='p-2 break-words whitespace-break-spaces kalam-regular'>
+                                {note.description}
+                            </h6>
+                        </div>
+                    </div>
+                )
+                }
+            </div>
+        </div >
+    )
+}
+
+export default StickyNotes
